e901c30f2380d8bffd06b6f253c65cf2d8baf55f
simenggg <2841583540lsm@gmail.com>
2025-01-25 17:42:53 +0800

    echo and bye

--- src/main/java/Lemon.java
+++ src/main/java/Lemon.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lemon {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,12 +8,30 @@ public class Lemon {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("——————————————————————————————————————————————");
         System.out.println("Hello! I'm Lemon");
         System.out.println("What can I do for you?");
+        System.out.println("——————————————————————————————————————————————");
 
-        System.out.println("——————————————————————————————————————————————");
-        System.out.println("Bye! Hope to see you again soon!");
-        System.out.println("——————————————————————————————————————————————");
+        while(true) {
+            String userInput = scanner.nextLine();
+            if(userInput.equals("bye")) {
+                System.out.println("——————————————————————————————————————————————");
+                System.out.println("Bye! Have a nice day and hope to see you again soon!");
+                System.out.println("——————————————————————————————————————————————");
+                break;
+            } else {
+                System.out.println("——————————————————————————————————————————————");
+                System.out.println(userInput);
+                System.out.println("——————————————————————————————————————————————");
+            }
+        }
+
+
+
+
     }
 }
